41acec6d355a84a6bc511aba9cea0c531198b1fc
shhady <shhadyse@gmail.com>
2022-04-06 23:28:46 +0300

    siblings

---
+++ app.js
@@ -0,0 +1,18 @@
+let sibling = parseInt(prompt('How many siblings do you have?')); {
+
+if (sibling === 1){
+    console.log ('1 Sibling');
+}
+else if (sibling > 1) {
+    console.log  ('more than 1 Sibling');
+}
+else {
+    console.log  ('No Siblings');
+    }
+}
+
+
+// with === getting No Siblings
+// with parseInt ();
+// to take the value and the type.
+
